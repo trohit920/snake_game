365efadd0e3f83680cdc8ff21492d9616d567e52
trohit920 <trohit920@gmail.com>
2022-01-06 15:15:51 +0900

    changed food object and random location

--- food.py
+++ food.py
@@ -5,14 +5,14 @@ import random
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.shape("turtle")
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-        self.color("blue")
+        self.color("green")
         self.speed("fastest")
         self.refresh()
 
     def refresh(self):
-        random_x = random.randint(-280, 280)
-        random_y = random.randint(-280, 280)
+        random_x = random.randint(-285, 285)
+        random_y = random.randint(-285, 285)
         self.goto(random_x, random_y)
